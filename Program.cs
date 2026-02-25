efce931ea306150e58a803564a80549888146b15
ST1047510 <ST10475105@rcconnect.edu.za>
2026-02-25 12:19:11 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections;
+
+namespace ASCIIvalue
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            //declare arraylist
+            ArrayList charlist = new ArrayList();
+
+            //Prompt user to enter 8 characters
+            Console.WriteLine("Hi Please enter 8 Characters: ");
+
+            for(int i = 0;i < 8; i++)
+            {
+                Console.WriteLine("character" +  (i  +  1)  +  ": ");
+                char character = Convert.ToChar(Console.ReadLine());
+                charlist.Add(character);
+            }
+            //Display ASCII Values
+            Console.WriteLine("\nASCII Values");
+
+            foreach(char character in charlist)
+            {
+                int ascii = (int)character;
+                Console.WriteLine("Character: "  + character +   "ASCII Value: "  + ascii);
+            }
+            Console.ReadLine();
+
+        }
+
+    }
+}
